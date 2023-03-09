f084c4971e7582d9f2b5a4d203d8aadd44551c66
vuongnd99 <nguyenvuong29599@gmail.com>
2023-03-08 21:20:58 -0600

    add test code to read data

--- utils/datautils.py
+++ utils/datautils.py
@@ -1,15 +1,17 @@
 import pyedflib
 import numpy as np
-from scipy import signal
+from scipy import signal, io 
 from scipy.signal import butter, filtfilt
 from sklearn.decomposition import FastICA
 from sklearn.preprocessing import scale
 import padasip as pa
+import pandas as pd
+import os
 
 class DataUtils:
 
     def __init__(self) -> None:
-        self.dataset = ["adbecg/", "nifecg/", "synt_ecg/"]
+        self.dataset = ["a&decgdb/", "nifecg/", "synt_ecg/"]
         self.record = '/RECORDS' # each dataset has a RECORDs file which contains file names
     """
     Read data from edf files. 3 Datasets contains:
@@ -24,27 +26,49 @@ class DataUtils:
     - Synthesized dataset: to be updated
     """
     def readData(self, dataset, path='data/'):
-        dataset_path = path + dataset
-        # if dataset == 'adbecg':
-        fileNames = dataset_path + self.record 
-        with open(fileNames, 'rb') as f:
-            files = f.readlines()
-        for file in files:
-            signal_path = dataset_path + file[:-1]
-            f = pyedflib.EdfReader(file_name)
-            n = f.signals_in_file
-    
-            adbECG = np.zeros((n-1, f.getNSamples()[0]))
-            fetalECG = np.zeros((1, f.getNSamples()[0]))
-            fetalECG[0, :] = f.readSignal(0)
-            fetalECG[0, :] = scale(self.butter_bandpass_filter(fetalECG, 1, 100, 1000), axis=1)
-            for i in np.arange(1, n):
-                adbECG[i - 1, :] = f.readSignal(i)
-    
-            adbECG = scale(self.butter_bandpass_filter(adbECG, 1, 100, 1000), axis=1)
-            adbECG = signal.resample(adbECG, int(adbECG.shape[1] / 5), axis=1)
-            fetalECG = signal.resample(fetalECG, int(fetalECG.shape[1] / 5), axis=1)
-            return adbECG, fetalECG
+        
+        if dataset == 'nifecg':
+            dataset_path = path + dataset
+            fileNames = dataset_path + self.record 
+            with open(fileNames, 'rb') as f:
+                files = f.readlines()
+            aECG = np.zeros((n-1, f.getNSamples()[0], len(files)))
+            fECG = np.zeros((1, f.getNSamples()[0]), len(files))
+            for i, file in enumerate(files):
+                signal_path = dataset_path + file[:-1]
+                f = pyedflib.EdfReader(signal_path)
+                n = f.signals_in_file                
+                fECG[0, :, i] = f.readSignal(0)
+                fECG[0, :, i] = scale(self.butter_bandpass_filter(fECG[0, :, i], 1, 100, 1000), axis=1)
+                for j in np.arange(1, n):
+                    aECG[j - 1, :, i] = f.readSignal(j)
+
+                aECG[:, :, i] = scale(self.butter_bandpass_filter(aECG[:, :, i], 1, 100, 1000), axis=1)
+                aECG[:, :, i] = signal.resample(aECG[:, :, i], int(aECG.shape[1] / 5), axis=1)
+                fECG[:, :, i] = signal.resample(fECG[:, :, i], int(fECG.shape[1] / 5), axis=1)
+            return aECG, fECG
+        elif dataset == 'a&decgdb':
+            dataset_path = path + dataset
+            file_paths = []
+            for fileName in os.listdir(dataset_path):
+                file_paths.append(os.path.join(dataset_path, fileName))
+            n = len(file_paths)
+            fECG = np.zeros((1, 10001, n))
+            aECG = np.zeros((4, 10001, n))
+            for i, file_path in enumerate(file_paths):
+                data = pd.read_csv(file_path)
+                fECG[:, :, i] = data[1]
+                for j in range(4):
+                    aECG[j, : , i] = data[j+2]
+                fECG[0, :, i] = scale(self.butter_bandpass_filter(fECG[0, :, i], 1, 100, 1000), axis=1)
+                aECG[:, :, i] = scale(self.butter_bandpass_filter(aECG[:, :, i], 1, 100, 1000), axis=1)
+                aECG[:, :, i] = signal.resample(aECG[:, :, i], int(aECG.shape[1] / 5), axis=1)
+                fECG[:, :, i] = signal.resample(fECG[:, :, i], int(fECG.shape[1] / 5), axis=1)
+            return aECG, fECG
+        else:
+            dataset_path = path + 'fecgsyndb.mat'
+            f = io.loadmat(dataset_path)
+            
             
     def windowingSig(self, sig1, sig2, windowSize = 15):
         signalLen = sig2.shape[1]
